5cb2092d8ec34903899454bb354d60049ac873ed
Andy <Andy.8866@outlook.com>
2023-02-02 16:50:59 +0800

    客户意见

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,9 +1,12 @@
 import appJson from "./app.json";
 import welcomeJson from "./welcome.json";
+import suggestJson from "./suggest.json";
 
 export default function getAppJson(){
     // @ts-ignore
-    const json= JSON.stringify(appJson).replace("\"$welcomePage\"",JSON.stringify(welcomeJson));
-    console.log(json)
+    let json= JSON.stringify(appJson)
+    json=json.replace(/"\$welcomePage"/g,JSON.stringify(welcomeJson));
+    json=json.replace(/"\$suggestPage"/g,JSON.stringify(suggestJson));
+    // console.log(json)
     return JSON.parse(json);
 }
