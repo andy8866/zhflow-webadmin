ee0691ef91b892d01997314a6687153a0abcc49f
Andy <Andy.8866@outlook.com>
2023-02-03 15:12:34 +0800

    用户列表

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,14 +1,16 @@
-import appJson from "./app.json";
-import welcomeJson from "./welcome.json";
-import suggestJson from "./suggest.json";
-import appManageJson from "./appManage.json";
+import app from "./app.json";
+import welcome from "./welcome.json";
+import suggest from "./suggest.json";
+import appManage from "./appManage.json";
+import userList from "./userList.json";
 
 export default function getAppJson(){
     // @ts-ignore
-    let json= JSON.stringify(appJson)
-    json=json.replace(/"\$welcomePage"/g,JSON.stringify(welcomeJson));
-    json=json.replace(/"\$suggestPage"/g,JSON.stringify(suggestJson));
-    json=json.replace(/"\$appManage"/g,JSON.stringify(appManageJson));
+    let json= JSON.stringify(app)
+    json=json.replace(/"\$welcomePage"/g,JSON.stringify(welcome));
+    json=json.replace(/"\$suggestPage"/g,JSON.stringify(suggest));
+    json=json.replace(/"\$appManage"/g,JSON.stringify(appManage));
+    json=json.replace(/"\$userList"/g,JSON.stringify(userList));
     // console.log(json)
     return JSON.parse(json);
 }
