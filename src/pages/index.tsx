bd07b9ab5d6d28a164b3dc9638f8d685c2733543
Andy <Andy.8866@outlook.com>
2023-02-17 10:52:43 +0800

    优化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import index from "./index.json";
 import {getUrl} from "../utils/httpUtil";
-import {doHttp} from "../utils/http";
 
 export default async function getIndexJson() {
     // @ts-ignore
     let json = JSON.stringify(index)
-    const page = await doHttp(getUrl() + '/api/admin/uiPage/getUiByCode?code=asideBefore', 'GET', null, null);
-    json = json.replace(/"\$asideBefore"/g,JSON.stringify(page));
 
     json = json.replace(/"\$url"/g, getUrl());
 
